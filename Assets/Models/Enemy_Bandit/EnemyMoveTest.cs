3b4f13664d9aa9b05a0e9daeb4df52b6c767b595
Nebularswamp <Nebularswamp@users.noreply.github.com>
2019-04-02 19:22:56 -0400

    bandit animator
    he do the run

---
+++ Assets/Models/Enemy_Bandit/EnemyMoveTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveTest : MonoBehaviour
+{
+    Animator anim;
+
+    public float movespeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", movespeed);
+    }
+}
