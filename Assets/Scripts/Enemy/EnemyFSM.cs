31b712dfcfd16dd2c3158dd442810e7c94f86020
Adrian De Andrade <redfoxhint4@hotmail.com>
2019-04-03 16:34:06 -0400

    Added new state machine

---
+++ Assets/Scripts/Enemy/EnemyFSM.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum States
+{
+    IDLE, SEARCH, PATROL, ATTACK, CHASE
+}
+
+public class EnemyFSM : MonoBehaviour
+{
+    
+}
